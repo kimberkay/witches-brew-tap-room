416f47f6a7ee5b74b7936b243149266951e78b48
Kim Brannian <brannian@gmail.com>
2022-02-27 20:36:15 -0800

    add dependencies for gh pages

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './components/App';
-import firebase from "./firebase"
-import reportWebVitals from './reportWebVitals';
+
+
 
 
 ReactDOM.render(
@@ -14,7 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
